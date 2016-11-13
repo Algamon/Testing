30384ab2b8fc7c2266c53e12b6b99085aa7aca61
Algamon <official_gaifullin@mail.ru>
2016-11-14 02:08:43 +0500

    New version

--- TestingProject/src/Anno/Anno.java
+++ TestingProject/src/Anno/Anno.java
@@ -1,27 +1,39 @@
 package Anno;
 
 import java.lang.annotation.*;
+import java.lang.reflect.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @interface Test{
 	String str() default "Testingmode";
 }
 @Test
-class TestAnno{
-	void One(){
+class Anno {
+	@Test(str="Method")
+	public static void one(){
+		Anno x = new Anno();
 		System.out.println("One");
-
-	}
-}
-public class Anno {
-
-	public static void main(String[] args) {
-		TestAnno x = new TestAnno();
-		x.One();
 		Annotation one_anno[]=x.getClass().getAnnotations();
 		for(Annotation a: one_anno){
 			System.out.println(a);
 		}
+		try{
+			Method one_meth=x.getClass().getMethod("one");
+			System.out.println(one_meth);
+		}
+		catch(Throwable abc){
+			System.out.println("Method not found "+ abc);
+		}
 	}
 
+
+
+	public static void main(String[] args) throws Exception {
+		//TestAnno x = new TestAnno();
+		one();
+		
+
+	
+
+}
 }
