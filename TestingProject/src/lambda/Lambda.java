6d5abf01c7001bda327805ce34d8ded14064d1cb
Algamon <official_gaifullin@mail.ru>
2016-11-24 18:42:03 +0500

    @lambda

--- TestingProject/src/lambda/Lambda.java
+++ TestingProject/src/lambda/Lambda.java
@@ -1,5 +1,5 @@
 package lambda;
-
+//тест лямбда-выражений
 interface Lam<T>{
 	T lam1(T n);
 }
@@ -12,9 +12,37 @@ public class Lambda {
 		return sf.lam1("Тестовая строка");
 	}
 	public static void main(String s[]) {
+		String y = "Раз раз, раз два три четыре восемь!";
 		System.out.println(lol(n -> Math.sqrt(n)));
 		System.out.println(str(n -> n.toUpperCase()));
-		//short b = 3;
+		y = funk_Op(Jazz_Funk::reverse_static,y);
+		System.out.println(y);
+		Jazz_Funk junk = new Jazz_Funk();
+		y = funk_Op(junk::reverse, y);
+		System.out.println(y);
 	}
+	static String funk_Op(Funk abc, String s){
+		return abc.funk(s);
+	}
+}
+interface Funk{
+	String funk(String n);
+}
 
+class Jazz_Funk{
+	static String reverse_static(String str){
+		String result ="";
+		for(int i=str.length()-1;i>=0;i--){
+			result += str.charAt(i);
+		}
+		return result;
+	}
+	String reverse(String str)	{
+		String result ="";
+		for(int i=str.length()-1;i>=0;i--){
+			result += str.charAt(i);
+		}
+		return result;
+	}
+	
 }
