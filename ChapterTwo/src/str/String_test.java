42003df92086b0e02592f2f0e6a8e6c33f954c03
Algamon <official_gaifullin@mail.ru>
2016-11-24 22:58:24 +0500

    str v1.0

--- ChapterTwo/src/str/String_test.java
+++ ChapterTwo/src/str/String_test.java
@@ -1,5 +1,29 @@
 package str;
 
-public class String_test {
+class Stringer{
+	Stringer(){
+		
+	}
+	public String toString(){
+		return "Override method toString()";
+	}
+}
 
+public class String_test {
+	public static void main(String... args){
+		{	//test override toString()
+			Stringer stger = new Stringer();
+			System.out.println(stger.toString());
+		}
+		{	//test Search in String
+			String str = "Testing text one: Children of Bodom - Hate Me, In Flames - Take this life, Marilyn Manson - Warship my wreck ";
+			int find = str.indexOf("Bodom");
+			char a[] = new char[10];
+			str.getChars(find, find+5, a, 0);
+			for (int x=0;x<a.length;x++){
+				System.out.print(a[x]);
+			}
+		}
+		
+	}
 }
